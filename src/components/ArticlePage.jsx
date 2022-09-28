fcbbd8ca63afcfd5d12e08d2dd97c7ceb806ff14
Phil Jacks <phil.jacks@zeninternet.co.uk>
2022-09-28 09:12:40 +0100

    Added loading indicator to article page

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -9,14 +9,19 @@ import "./ArticlePage.css";
 const ArticlePage = () => {
   const [article, setArticle] = useState({});
   const { article_id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     getArticleById(article_id)
       .then(({ data }) => {
         setArticle(data.article);
+        setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, [article_id, article]);
+  }, [article_id]);
+
+  if (isLoading) return <p>Loading article...</p>;
 
   return (
     <>
