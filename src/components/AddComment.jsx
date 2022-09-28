f6151d81d878e383e8d4c13805b828dd86c49c07
Phil Jacks <phil.jacks@zeninternet.co.uk>
2022-09-28 12:14:39 +0100

    Add comment to indivual articles

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { postCommentByArticleId } from "../api/requests";
+
+import "./AddComment.css";
+
+const AddComment = ({ article_id, comments, setComments }) => {
+  const [comment, setComment] = useState("");
+  const [msg, setMsg] = useState("");
+  const [msgColor, setMsgColor] = useState("");
+  const [isPending, setIsPending] = useState(false);
+
+  const commentDisabled = isPending ? true : false;
+
+  const postNewComment = (e) => {
+    e.preventDefault();
+
+    const newCommentUI = {
+      body: comment,
+      votes: 0,
+      author: "weegembump",
+      article_id: article_id,
+      created_at: Date.now(),
+      comment_id: comments.length + 1,
+    };
+
+    setComments((currComments) => {
+      return [...currComments, newCommentUI];
+    });
+
+    setMsg("Adding comment...");
+    setIsPending(true);
+
+    postCommentByArticleId(article_id, {
+      username: "weegembump",
+      body: comment,
+    })
+      .then((res) => {
+        setMsgColor("green");
+        setMsg("Comment added");
+        setIsPending(false);
+        setTimeout(() => {
+          setMsg("");
+          setMsgColor("");
+        }, 3000);
+      })
+      .catch((err) => {
+        if (err) {
+          setMsgColor("red");
+          setMsg("Sorry, something went wrong");
+          setIsPending(false);
+          setTimeout(() => {
+            setMsg("");
+            setMsgColor("");
+          }, 3000);
+          setComments((currComments) => {
+            return currComments.map((comment, index) => {
+              if (comment.comment_id === newCommentUI.comment_id) {
+                currComments.splice(index, 1);
+              }
+              return comment;
+            });
+          });
+        }
+      });
+
+    setComment("");
+  };
+
+  const clearComment = () => {
+    setComment("");
+  };
+
+  return (
+    <form onSubmit={postNewComment} className="add-comment">
+      <textarea
+        required
+        onInput={(e) => {
+          setComment(e.target.value);
+        }}
+        name="comment"
+        id="comment"
+        placeholder="Add a comment..."
+        value={comment}
+        disabled={commentDisabled}
+      ></textarea>
+      <div className="btns">
+        <button type="reset" onClick={clearComment} className="cancel-btn">
+          Cancel
+        </button>
+        <button className="submit-btn">Comment</button>
+      </div>
+      <p className={`${msgColor} msg`}>{msg}</p>
+    </form>
+  );
+};
+
+export default AddComment;
--- src/components/ArticleComments.jsx
+++ src/components/ArticleComments.jsx
@@ -1,20 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { getCommentsByArticleId } from "../api/requests";
 import CommentCard from "./CommentCard";
+import AddComment from "./AddComment";
 
 import "./ArticleComments.css";
 
 const ArticleComments = ({ article_id }) => {
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     getCommentsByArticleId(article_id)
-      .then(({ data }) => setComments(data.comments))
+      .then(({ data }) => {
+        setComments(data.comments);
+        setIsLoading(false);
+      })
       .catch((err) => console.log(err));
   }, [article_id]);
+
+  if (isLoading) return <p>Loading comments...</p>;
+
   return (
     <section className="comments-list">
       <h3>Comments</h3>
+      <AddComment
+        article_id={article_id}
+        comments={comments}
+        setComments={setComments}
+      />
       <ul>
         {comments.map((comment) => {
           return <CommentCard key={comment.comment_id} comment={comment} />;
